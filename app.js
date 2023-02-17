3331177ddfd33f3f21d32cfe48f2a617ffed4b76
koddiyari.com <omers4hin04@gmail.com>
2023-02-17 15:33:19 +0300

    MVC YAPISI

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 import express from "express";
 import ejs from "ejs";
 import dotenv from "dotenv";
-
 import conn from "./db.js"
-
+import pageRoute from "./routes/pageRoute.js"
 
 
 
@@ -17,12 +16,8 @@ const host = "127.0.0.1";
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.render("index");
-});
-app.get("/about", (req, res) => {
-  res.render("about");
-});
+app.use("/",pageRoute);
+
 
 app.listen(port, host, () => {
   console.log(`Port Dinleniyor http://${host}:${port}  `);
